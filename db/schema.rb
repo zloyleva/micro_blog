00bb1def8e2459a73f90e287d872c3e23e6778ab
zloyleva <zloyleva@gmail.com>
2016-11-04 00:18:50 +0200

    Create User table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161103125042) do
+ActiveRecord::Schema.define(version: 20161103221637) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -23,6 +23,15 @@ ActiveRecord::Schema.define(version: 20161103125042) do
 
   create_table "posts", force: :cascade do |t|
     t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "name"
+    t.string   "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
