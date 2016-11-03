cf373272e7ea57afb25b80cd848cc3f205609ddb
zloyleva <zloyleva@gmail.com>
2016-11-04 00:35:55 +0200

    Add destroy routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  root 'home#index'
 
   match "/auth/:provider/callback" => "sessions#create", :via => [:get, :post]
-  
+  match "/signout" => "sessions#destroy", :as => :signout, :via => [:delete]
 
-  root 'posts#index'
+
+#  root 'posts#index'
 
   resources :posts do
     resources :comments
